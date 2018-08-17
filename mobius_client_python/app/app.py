42a499daf2e98fa2f8821a81fb097bc8c1452dc3
Your Name <you@example.com>
2018-08-17 13:26:29 +0200

    Passing in network to payment op

--- mobius_client_python/app/app.py
+++ mobius_client_python/app/app.py
@@ -7,6 +7,7 @@ class App(object):
     def __init__(self, app_account, user_account, network=None):
         self.app_account = app_account
         self.user_account = user_account
+        self.network = network
         self.client_instance = Client(network).get_horizon_client()
 
     def authorized(self):
@@ -109,14 +110,14 @@ class App(object):
     def user_payment_op(self, amount, destination):
         return Payment(opts={'destination': destination,
                              'amount': str(amount),
-                             'asset': Client().get_stellar_asset(),
+                             'asset': Client(self.network).get_stellar_asset(),
                              'source': self.user_keypair().address().decode()
                              })
 
     def app_payment_op(self, amount, destination):
         return Payment(opts={'destination': destination,
                              'amount': str(amount),
-                             'asset': Client().get_stellar_asset(),
+                             'asset': Client(self.network).get_stellar_asset(),
                              'source': self.app_keypair().address().decode()
                             })
 
